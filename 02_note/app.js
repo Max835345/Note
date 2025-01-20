d2df9d3907e6bb70f087463f55811aaa833c1108
Max <79370059+Max835345@users.noreply.github.com>
2025-01-20 22:01:51 +0300

    Update app.js

--- 02_note/app.js
+++ 02_note/app.js
@@ -2,34 +2,6 @@ const inputElement = document.getElementById('title')
 const craeteBtn = document.getElementById('create')
 const listElement = document.getElementById('list')
 
-// function render() {
-//     listElement.insertAdjacentHTML('beforeend',getNoteTemplate(notes[0]))
-// }
-
-// render()
-
-// craeteBtn.onclick = function () {
-//     if (inputElement.value.length === 0){
-//         return 
-//     }
-
-//     listElement.insertAdjacentHTML('beforeend',getNoteTemplate(inputElement.value))
-  
-//     inputElement.value = ''
-// }
-
-// function getNoteTemplate(title) {
-//     return `
-//     <li class="list-group-item d-flex justify-content-between align-items-center">
-//     <span>${title}</span>
-//     <span>
-//       <span class="btn btn-small btn-success">&check;</span>
-//       <span class="btn btn-small btn-danger">&times;</span>
-//     </span>
-//     </li>
-//     `
-// }
-
 let notes = [
     {
         title:'XYU',
